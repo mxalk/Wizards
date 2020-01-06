58400a862f5aafdbb5354641046156bcbf2e01f4
Manolis Chalkiadakis <manos.ch96@gmail.com>
2020-01-06 22:01:53 +0200

    Logical spell actions and loops missing

--- Hogwards.h
+++ Hogwards.h
@@ -7,6 +7,7 @@
 #include "classes/Wizard.h"
 #include "classes/Spell.h"
 #include "classes/Game.h"
+#include <iostream>
 
 #define BEGIN_GAME int main() {Wizard tmpw; Spell tmps;
 #define END_GAME return 0;}
@@ -37,7 +38,22 @@
 #define DAMAGE ;
 #define HEAL ;
 #define EQUIP ;
+
 #define GET_HP ;Game::get_hp
 #define GET_HOUSE ;Game::get_house
 #define GET_NAME ;Game::get_name
 #define HAS_WAND ;Game::has_wand
+
+#define AND Game::and_f
+#define OR Game::or_f
+#define NOT Game::not_f
+
+#define IF if (
+#define ELSE_IF else if (
+#define ELSE else {
+#define DO ) {
+
+#define FOR for (int i=0; i<
+#define ROUNDS ; i++
+
+#define SHOW cout<<
--- classes/Game.h
+++ classes/Game.h
@@ -4,11 +4,27 @@
 #pragma once
 
 #include "classes/Wizard.h";
+#include <stdarg.h>
 
 class Game {
 public:
-    static int get_hp(Wizard w){ return w.hp; };
+    static int get_hp(Wizard w) { return w.hp; };
     static string get_house(Wizard w) { return w.house; }
     static string get_name(Wizard w) { return w.name; }
     static bool has_wand(Wizard w) { return w.wand; }
+
+    static bool and_f(bool arg1, bool arg2, bool...) {
+        bool tmp = true;
+        if (!arg1 || !arg2) return false;
+        va_list vl;
+        va_start(vl,tmp);
+        while (tmp) {
+            if (!tmp) return false;
+            tmp=va_arg(vl, bool);
+        }
+        va_end(vl);
+        return true;
+    };
+    static bool or_f(bool arg1, bool arg2, bool...) {};
+    static bool not_f(bool arg) { return !arg; };
 };
