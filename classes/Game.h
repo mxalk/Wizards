cd00e50bc349183b17beab974b8de86aab2fccf4
Antonis Tap <csd3465@csd.uoc.gr>
2020-01-08 06:28:05 +0200

    a try to fix logical and/or

--- classes/Game.h
+++ classes/Game.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "classes/Wizard.h"
-#include <stdarg.h>
+#include <cstdarg>
 
 class Game {
 public:
@@ -13,20 +13,43 @@ public:
     static string get_name(Wizard w) { return w.name; }
     static bool has_wand(Wizard w) { return w.wand; }
 
-    static bool and_f(bool arg1, bool arg2, bool...) {
-        bool tmp = true;
+    /*
+     * an sinexizei na prospa8ei na diavazei arguments
+     * akoma ki an den uparxoun, tote yparxei provlima.
+     * Pou isos na linete me kapoio try catch.
+     * Allios an iparxei kapoios tropos na ta metrame,
+     * px gia ka8e comma pou vriskei +1 argument kai na valoume
+     * auton ton counter san proto orisma stin sinartisi as poume
+     *
+     * an den ginete tpt apo auta, mporoume na to afisoume
+     * to "toulaxiston 2" kai na pai3oume me "akrivos 2" orismata
+     * gia tin ora, kai to koitame argotera mipos skeftoume
+     * kalitero tropo
+     * */
+
+    static bool and_f(bool arg1, bool arg2...) {
         if (!arg1 || !arg2) return false;
         va_list vl;
-        va_start(vl,tmp);
-        while (tmp) {
-            if (!tmp) return false;
-            tmp=va_arg(vl, bool);
+        va_start(vl,arg2);
+        bool tmp = true;
+        while(tmp) {
+            tmp = va_arg(vl, bool);
         }
         va_end(vl);
-        return true;
+        return tmp;
     };
-    static bool or_f(bool arg1, bool arg2, bool...) {
 
+    static bool or_f(bool arg1, bool arg2, bool...) {
+        if (arg1 || arg2) return true;
+        va_list vl;
+        va_start(vl,arg2);
+        bool tmp = false;
+        while(!tmp) {
+            tmp = va_arg(vl, bool);
+        }
+        va_end(vl);
+        return tmp;
     };
+
     static bool not_f(bool arg) { return !arg; };
 };
