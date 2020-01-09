c45bf89b35daadd920219c8cc8fd47551dcc267e
Antonis Tap <csd3465@csd.uoc.gr>
2020-01-09 02:05:27 +0200

    duel() pseudo

--- game.cpp
+++ game.cpp
@@ -66,3 +66,74 @@ BEGIN_GAME
 //DUEL
 Game::printall();
 END_GAME
+
+/*
+#define DASHES std::cout<<"------------------"<<endl
+#define TILDES std::cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl
+#define HASHES std::cout<<"##################"<<endl
+
+void duel() {
+
+        std::string str1;
+        std::string str2;
+
+        cout << "-------------------------HARRY POTTER THE GAME------------------------" << endl << endl;
+        cout << "Player1 select wizard:" << endl;
+        DASHES;
+        Wizard::print_wizards_names();
+        DASHES;
+        getline (std::cin, str1);
+
+        cout << "Player2 select wizard:" << endl;
+        DASHES;
+        Wizard::print_wizards_names();
+        DASHES;
+        getline (std::cin, str2);
+
+        Wizard w1 = Wizard::getWizard(str1);
+        
+        if (str1.compare(str2) != 0) { 
+                Wizard w2 = Wizard::getWizard(str2);
+        } else {
+                // copy constructor
+        } 
+
+        while ( game::get_hp(w1) > 0 && game::get_hp(w2) > 0) {
+                print_round(  );
+                
+                // if(player1.wand) {
+                //      Spell sp1
+                //      string s1
+                //      spell_select(player1)
+                //      players_status()
+                // } else print_no_wand_msg
+                
+                // if(player2.wand) {
+                //      Spell sp2
+                //      string s2     
+                //      spell_select(player2)
+                //      players_status()
+                // } else print_no_wand_msg
+
+                // round++ 
+        }
+
+} 
+
+void print_round(int round) {
+        TILDES;
+        cout << "Round " << round << endl;
+        TILDES;
+}
+
+/////////////////////
+spell_select () {
+        cout << name_1 << ...... ;
+        DASHES;
+        print_w1_spell_list()         
+        DASHES;
+        getline (std::cin, s1);
+        sp1 = Wizard::getSpell(s1);  
+}
+
+*/
